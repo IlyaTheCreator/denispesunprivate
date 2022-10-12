1eecdb8493f4f58881350d85f234c6b09cde284c
ilya klimov <respondatanytime@gmail.com>
2022-10-12 10:40:38 +0300

    DP-2: Компонент AppHeader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,11 @@
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setName } from '../../store/reducers/root.reducer';
+import AppHeader from '../../components/AppHeader/AppHeader';
+
 import styles from './Home.module.scss';
 
 const Home = () => {
-  const name = useSelector(store => store.root.name);
-  const dispatch = useDispatch();
-
   return (
     <div className={styles['home-page']}>
-      <h1>Home, {name}</h1>
-      <button onClick={() => dispatch(setName('ivan'))}>
-        change name
-      </button>
-      <Link to="/about">about page</Link>
+      <AppHeader />
     </div>
   );
 };
