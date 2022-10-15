035cf43c7d1e1363ee9e0f03fad502225298e6b4
MrArtga <artga03@gmail.com>
2022-10-15 18:32:35 +0300

    DP-7: Сделал примерно как на картинке

--- src/components/HeaderImage/HeaderImage.jsx
+++ src/components/HeaderImage/HeaderImage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './HeaderImage.module.scss';
 
 const HeaderImage = ({ url }) => {
@@ -5,7 +6,29 @@ const HeaderImage = ({ url }) => {
     backgroundImage: `url(${url})`,
   };
 
-  return <div style={divStyles} className={styles['header-image']} />;
+  return (
+    <div style={divStyles} className={styles['header-image']}>
+      <div className="container">
+        <div className={styles.container_content}>
+          <div className={styles.contents}>
+            <div className={styles.header_contents}>
+              {/* здеся будут кнопки */}
+            </div>
+            <div className={styles.body_contents}>
+              <h4>Travels</h4>
+              <h1>When Is The Best Time of Year To Visit Japan?</h1>
+              <span>
+                The good news for travelers is that there is no single best time of year to travel to Japan...
+              </span>
+              <Link to="/article">
+                <button>Read more</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeaderImage;
