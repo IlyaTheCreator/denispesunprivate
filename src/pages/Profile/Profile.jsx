7d112da4a7c0066aa150fc0322786155e7dc56cd
ilya klimov <respondatanytime@gmail.com>
2022-10-17 09:27:19 +0300

    DP-11: Стили и моковое наполнение для страницы профайла

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,6 @@
 import MockPageWrapper from '../../components/MockPageWrapper/MockPageWrapper';
+import ArticleItem from '../../components/ArticleItem/ArticleItem';
+import { MOCK_ARTICLES_LIST } from '../../constants/mocks';
 import styles from './Profile.module.scss';
 
 const Profile = () => {
@@ -6,7 +8,18 @@ const Profile = () => {
     <MockPageWrapper>
       <div className="page">
         <div className="container">
-          <div className={styles.profile}>Profile</div>
+          <div className={styles.profile}>
+            <div className={styles['user-info']}>
+              <h1>Username: Ivan Petrov</h1>
+              <p>Date registered: 08.08.2003</p>
+            </div>
+            <h1 className={styles.header}>Your articles</h1>
+            <div className={styles['user-articles']}>
+              {MOCK_ARTICLES_LIST.slice(0, 7).map(item => (
+                <ArticleItem key={item.title} data={item} isProfile />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </MockPageWrapper>
